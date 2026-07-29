cebd2f9ac7a0445750956471de8b0b55d3824eee
dheerajsaini01 <tanwardheeraj00111@gmail.com>
2026-07-29 12:02:08 +0530

    Add comments to clarify the functionality of Hugging Face chat model integration

--- ChatModels/chatmodel_hf_api.py
+++ ChatModels/chatmodel_hf_api.py
@@ -1,3 +1,8 @@
+# 1. Simple Text vs. Chat Messages (Roles)HuggingFaceEndpoint:
+# Yeh ek raw text-completion model hai. Yeh nahi samajhta ki SystemMessage, HumanMessage, ya AIResponse kya hote hain. Yeh bas ek lamba string leta hai aur aage ka text predict kar deta hai.
+# ChatHuggingFace: Yeh is raw model ke upar ek wrapper (layer) laga deta hai. Yeh aapke input ko Llama-3.1 ke specific chat format (jaise <|start_header_id|>user<|end_header_id|>) mein convert karta hai.
+
+
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import os
@@ -11,7 +16,7 @@ llm = HuggingFaceEndpoint(
     huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
 )
 
-model = ChatHuggingFace(llm=llm)
+model = ChatHuggingFace(llm=llm) #Yeh line basically aapke simple text model ko ek smart Chat Model mein badal deti hai jo Hugging Face ke standard patterns ko samajh sake.
 
 result = model.invoke("What is Html?")
 
